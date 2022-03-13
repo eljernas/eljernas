f8f9710893c6eee9bf2f60f9ec751ee3c8f07f38
eljernas <91350258+eljernas@users.noreply.github.com>
2022-03-13 22:42:34 +0300

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,48 @@
+const but = document.querySelectorAll(".but");
+const val = document.getElementsByClassName("vinput")[0];
+const rest = document.getElementsByClassName("off")[0];
+const del = document.getElementsByClassName("del")[0];
+const equal = document.getElementsByClassName("equal")[0];
+document.onkeydown = () => {
+    const reg = /[a-zA-Z]/.test(val.value);
+
+    if (reg == true) {
+        val.value = "";
+    }
+}
+
+// value buttons
+
+but.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+
+        // console.log(item.value)
+        val.value += item.value;
+    })
+});
+// rest button
+rest.addEventListener("click", () => {
+    val.value = "";
+});
+
+// delete one value
+del.addEventListener("click", () => {
+    console.log(val.value.length - 1);
+    const result = val.value.length - 1
+    val.value = val.value.slice(0, result);
+});
+
+// result
+equal.addEventListener("click", () => {
+    const reg = /[a-zA-Z]/.test(val.value);
+
+    if (reg == true) {
+        val.value = "";
+    }else{
+    val.value = eval(val.value);
+    }
+});
+
+
